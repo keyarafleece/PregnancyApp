a7985d752f72f3a999e6fd771d69cecd16ebec75
keyar <keyarafleece@gmail.com>
2022-03-09 17:58:49 -0500

    Added nav graph, created Kotlin files for each XML file, added logic for all onboarding screen buttons to connect, removed main activity from navigation and changed to container for navigation, created WelcomeFragment.kt to be the opening fragment.

---
+++ app/src/main/java/com/example/pregnancy_app/WelcomeFragment.kt
@@ -0,0 +1,44 @@
+package com.example.pregnancy_app
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+
+
+
+
+class WelcomeFragment : Fragment() {
+
+
+    /* override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+     */
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_welcome, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        view.findViewById<Button>(R.id.button_activity_main).setOnClickListener { view ->
+            view.findNavController().navigate(R.id.action_welcomeFragment_to_onboardingOne)
+        }
+
+
+
+                }
+            }
